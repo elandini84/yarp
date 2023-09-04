1efcecf0cfdd3f772a818c06ed8915781a973f23
Ettore Landini <ettore.landini@iit.it>
2023-09-04 15:26:16 +0200

    Added test files for the IChatBot interface

---
+++ src/libYARP_dev/src/yarp/dev/tests/IChatBotTest.h
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2023 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef ICHATBOTTEST_H
+#define ICHATBOTTEST_H
+
+#include <yarp/dev/IChatBot.h>
+#include <catch2/catch_amalgamated.hpp>
+
+using namespace yarp::dev;
+
+namespace yarp::dev::tests {
+inline void exec_iChatBot_test_1(yarp::dev::IChatBot* ichatbot)
+{
+    REQUIRE(ichatbot != nullptr);
+
+    bool b;
+
+    b = ichatbot->setLanguage("eng");
+    CHECK(b);
+
+    std::string tempLang;
+    b = ichatbot->getLanguage(tempLang);
+    CHECK(b);
+    CHECK(tempLang == std::string("eng"));
+
+    b = ichatbot->resetBot();
+    CHECK(b);
+
+    std::string messageIn{"Hello!"};
+    std::string messageOut;
+    b = ichatbot->interact(messageIn,messageOut);
+    CHECK(b);
+
+    std::string tempBackUp;
+    b = ichatbot->backupBot(tempBackUp);
+    CHECK(b);
+
+    std::string tempBackUp2;
+    b = ichatbot->restoreBot(tempBackUp);
+    CHECK(b);
+    ichatbot->backupBot(tempBackUp2);
+    CHECK(tempBackUp2 == tempBackUp);
+}
+}
+
+#endif //ICHATBOTTEST_H
